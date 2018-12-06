265917dbf123aa4776e2e6c9cc592ef233561c2f
Akos Polster <akos@pipacs.com>
2018-12-06 22:18:28 +0100

    .

--- looper/topic_panda.py
+++ looper/topic_panda.py
@@ -1,7 +1,11 @@
 # Copyright (c) Akos Polster. All rights reserved.
 
+import random
 from PIL import Image
 
+image_names = ["xmas.png", "panda.png"]
+
 def topic_panda():
-    image = Image.open("looper/panda.png")
+    image_name = "looper/" + random.SystemRandom().choice(image_names)
+    image = Image.open(image_name)
     return (None, None, image)
